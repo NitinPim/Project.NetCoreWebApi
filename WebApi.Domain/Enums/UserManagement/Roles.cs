8a4ca6bff3ee85fd3c0b9e23a4e4eb78790d82e8
Nitin Pimpalkar <nitinpimpalkar17@gmail.com>
2025-06-16 12:37:54 +0530

    Roles Enum

---
+++ WebApi.Domain/Enums/UserManagement/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace WebApi.Domain.Enums.UserManagement
+{
+    public enum Roles
+    {
+        [Description("Admin")]
+        Admin = 1,
+
+        [Description("App User")]
+        AppUser = 2
+    }
+}
