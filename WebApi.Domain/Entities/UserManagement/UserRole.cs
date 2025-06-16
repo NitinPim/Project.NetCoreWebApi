b96c978c08588f80248411c34a86534cf2ae5269
Nitin Pimpalkar <nitinpimpalkar17@gmail.com>
2025-06-16 11:54:10 +0530

    Commit

--- WebApi.Domain/Entities/UserManagement/UserRole.cs
+++ WebApi.Domain/Entities/UserManagement/UserRole.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebApi.Domain.Entities.UserManagement
+﻿namespace WebApi.Domain.Entities.UserManagement
 {
-    internal class UserRole
+    public class UserRole
     {
     }
 }
