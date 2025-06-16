06d49868e9c1f3cca94266ea6927ffc8acb2b0b9
Nitin Pimpalkar <nitinpimpalkar17@gmail.com>
2025-06-16 13:12:11 +0530

    Added RequestModel

---
+++ Lib.Core/Models/Request/RequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lib.Core.Models.Request
+{
+    public class RequestModel
+    {
+        public string? AccessToken { get; set; }
+        public string? URI { get; set; }
+        public string? JsonData { get; set; }
+    }
+}
